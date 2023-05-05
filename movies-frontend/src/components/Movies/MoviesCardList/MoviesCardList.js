c761f6eb258cb224b48d51c647680162aac8e03b
Dmitriy Belozerov <dm.belozerov@yandex.ru>
2023-05-05 22:54:22 +0300

    to do search checkbox

--- movies-frontend/src/components/Movies/MoviesCardList/MoviesCardList.js
+++ movies-frontend/src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -6,7 +6,7 @@ function MoviesCardList(props) {
   return (
     <section className={`moviesCardList ${props.movies ? 'moviesCardList_hiden' : ''}`}>
       <ul className="moviesCardList__list">
-        {props.movies.map(item => (
+        {props.movies && props.movies.map(item => (
           <MoviesCard key={`props.movie ? ${item.id} : ${item._id}`} movieType={props.movie} title={item.nameRU} image={item.image.url}
             duration={item.duration} data={item} onMovieSave={props.handleSave} onMovieDelete={props.handleDelete} myMovies={props.myMovies}>
           </MoviesCard>
